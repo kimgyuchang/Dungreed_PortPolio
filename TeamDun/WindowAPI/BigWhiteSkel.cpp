335a536373f0780ed4071f0778ff6f575316bbbe
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2020-09-29 09:19:49 +0900

    근거리 몬스터 이미지 추가 & 화이트스컬 (BigWhiteSkel) 클래스 추가

---
+++ TeamDun/WindowAPI/BigWhiteSkel.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "BigWhiteSkel.h"
+
+HRESULT BigWhiteSkel::init(int id, string name, OBJECTTYPE type, vector<string> imgNames)
+{
+	return S_OK;
+}
+
+void BigWhiteSkel::update()
+{
+}
+
+void BigWhiteSkel::release()
+{
+}
+
+void BigWhiteSkel::render(HDC hdc)
+{
+}
+
+void BigWhiteSkel::Move()
+{
+}
+
+void BigWhiteSkel::Attack()
+{
+}
+
+void BigWhiteSkel::Animation()
+{
+}
---
+++ TeamDun/WindowAPI/BigWhiteSkel.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "enemy.h"
+class BigWhiteSkel : public Enemy
+{
+private:
+
+public:
+	virtual HRESULT init(int id, string name, OBJECTTYPE type, vector<string> imgNames);
+	virtual void	update();
+	virtual	void	release();
+	virtual void	render(HDC hdc);
+
+	virtual void	Move();
+	virtual void	Attack();
+	virtual void	Animation();
+};
+
