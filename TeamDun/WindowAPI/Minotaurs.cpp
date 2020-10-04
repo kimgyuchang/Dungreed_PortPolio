86195a65be6745d14c4faf65fb9eb07d9a5385f9
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2020-10-04 17:02:27 +0900

    맵 추가 (upSideDownStair) & 미노타우르스 일부 수정 & 보라박쥐 이미지 변경

--- TeamDun/WindowAPI/Minotaurs.cpp
+++ TeamDun/WindowAPI/Minotaurs.cpp
@@ -7,12 +7,12 @@ HRESULT Minotaurs::init(int id, string name, OBJECTTYPE type, vector<string> img
 	_body = RectMake(_x, _y, 156, 114);
 	_state = ES_IDLE;
 
-	_index = _count = _friction = 0;
+	_index = _count = _dashTimer = _dashCount = 0;
 	_frameX, _frameY = 0;
 
 	_moveSpeed = 15.0f;
 
-	_isLeft = _dash = false;
+	_isLeft = _isDash = _isAttack = false;
 	return S_OK;
 }
 
@@ -20,15 +20,47 @@ void Minotaurs::update()
 {
 	Enemy::update();
 
+	_dashTimer++;
+
 	if (_isSpawned)
 	{
 		switch (_state)
 		{
 		case ES_IDLE:
+			if (abs(_x - ENTITYMANAGER->getPlayer()->GetX()) < 300 && abs(_y - ENTITYMANAGER->getPlayer()->GetY()) < 50)
+			{
+				_state = ES_MOVE;
+			}
 			break;
 		case ES_MOVE:
+			if (ENTITYMANAGER->getPlayer()->GetX() > _x)
+			{
+				_isLeft = true;
+			}
+			else
+			{
+				_isLeft = false;
+			}
+			if (_isLeft && _frameX >= _vImages[_useImage]->getMaxFrameX())
+			{
+				_state = ES_ATTACK;
+			}
+			else if (!_isLeft && _frameX <= 0)
+			{
+				_state = ES_ATTACK;
+			}
 			break;
 		case ES_ATTACK:
+			if (_isLeft && _frameX >= _vImages[_useImage]->getMaxFrameX())
+			{
+				_state = ES_IDLE;
+				_isDash = false;
+			}
+			else if (!_isLeft && _frameX <= 0)
+			{
+				_state = ES_IDLE;
+				_isDash = false;
+			}
 			break;
 		default:
 			break;
@@ -55,23 +87,20 @@ void Minotaurs::Move()
 {
 	Enemy::Move();
 
-	if (abs(_x - ENTITYMANAGER->getPlayer()->GetX()) < 300)
+	if (_state == ES_MOVE)
 	{
-		_state = ES_MOVE;
-		_dash = true;
+		_isDash = true;
 	}
-	if (_x > ENTITYMANAGER->getPlayer()->GetX() && _dash)
-	{
-		_friction++;
-		_x -= _moveSpeed;
-		_moveSpeed -= _friction;
 
-		if (_moveSpeed <= 0)
+	if (_isDash && _dashCount == 0)
+	{
+		if (!_isLeft)
 		{
-			_moveSpeed = 15.0f;
-			_friction = 0;
-			_state = ES_IDLE;
-			_dash = false;
+			_x -= _moveSpeed;
+		}
+		else if (_isLeft)
+		{
+			_x += _moveSpeed;
 		}
 	}
 }
